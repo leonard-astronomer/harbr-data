0fc79828e40cc360355f9154c4d45e9faf51bef5
leonard-astronomer <leonard.walstad@astronomer.io>
2022-01-12 13:08:55 -0500

    Interim commit.  Almost working.

---
+++ include/Tracking.py
@@ -0,0 +1,52 @@
+from datetime import datetime
+
+class Tracking:
+    def __init__(self, name):
+        self.name = name
+        self.parameters = {'name': [], 'value': []}
+        self.end_time = 0
+        self.duration = 'Not set'
+        self.success = False
+        self.error_message = "Not set"
+        self.start_time = datetime.now()
+
+    def record_parameter(self, name, value):
+        self.parameters['name'].append(name)
+        self.parameters['value'].append(value)
+
+    def record_end_time(self):
+        self.end_time = datetime.now()
+        self.duration = self.end_time - self.start_time
+
+    def failed(self, message=""):
+        self.end_time = datetime.now()
+        self.duration = self.end_time - self.start_time
+        if (message != ""):
+            self.error_message = message
+        self.success = False
+
+    def succeeded(self):
+        self.end_time = datetime.now()
+        self.duration = self.end_time - self.start_time
+        self.success = True
+
+    def set_error_message(self, msg):
+        self.error_message = msg
+
+    def get_summary(self):
+        summary = "Tracking: Name:       {}\n".format(self.name)
+        summary += "          Success:    {}\n".format(self.success)
+        if (not self.success):
+            summary += "          Message:    {}\n".format(self.error_message)
+        n = len(self.parameters['name'])
+        if (n > 0):
+            summary += "          Parameters: Name \tValue\n"
+            for i in range(0, n):
+                summary += "                      {}\t\t{}\n".format(self.parameters['name'][i],
+                                                                     self.parameters['value'][i])
+
+        summary += "          Start:      {}\n".format(self.start_time)
+        summary += "          End:        {}\n".format(self.end_time)
+        summary += "          Duration:   {}\n".format(self.duration)
+
+        return summary
---
+++ test/test_check_harbr_endpoint_folder.py
@@ -0,0 +1,16 @@
+import boto3
+from harbr_utilities import tracking
+from harbr_utilities import examine_harbr_endpoint_folder
+
+
+session = boto3.Session(profile_name='default')
+s3 = session.client('s3')
+bucket = "walstad-astronomer"
+endpoint = "Wh8CGNSjQeWBbC0Q1j8XEA"
+folder = "singleload"
+
+track = tracking("Check for singleload folder.")
+contents = check_harbr_endpoint_folder(s3, bucket, endpoint, folder, track)
+if (not track.success):
+    print("Failed: \n{}")
+print("{}".format(track.get_summary()))
